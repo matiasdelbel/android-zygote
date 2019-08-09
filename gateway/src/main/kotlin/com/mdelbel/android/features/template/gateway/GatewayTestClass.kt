ab80a5e01a52ac807aa6495be6c06bc38bbd13d7
unknown <matiasdelbel@gmail.com>
2019-08-09 10:48:41 -0400

    Migrate to zygote

--- gateway/src/main/kotlin/com/mdelbel/android/features/template/gateway/GatewayTestClass.kt
+++ gateway/src/main/kotlin/com/mdelbel/android/features/template/gateway/GatewayTestClass.kt
@@ -1,8 +1,14 @@
 package com.mdelbel.android.features.template.gateway
 
+import com.mdelbel.android.features.template.domain.DomainTestClass
+
 // TODO Delete this class after clone
 data class GatewayTestClass(val value: Int) {
 
     fun addOneToValue() = GatewayTestClass(value + 1)
+
+    fun bla() {
+        val f = DomainTestClass(0)
+    }
 
 }
